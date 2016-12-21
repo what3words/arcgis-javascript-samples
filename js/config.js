9ab95279d7cbafcf642ebe34f8c568ae24892d49
arnaud ferrand <arferrand@gmail.com>
2016-12-21 16:37:50 +0000

    prompts for w3w API key and arcgis locator password

--- js/config.js
+++ js/config.js
@@ -1,8 +1,15 @@
 define([], function() {
+    // Get the user's w3w API key via prompt
+    if (!localStorage.getItem('w3wkey')) {
+      localStorage.setItem(
+        'w3wkey',
+        prompt('What is your w3w API key?')
+      );
+    }
   return {
     // sign up of for your api key, it is free at http://developer.what3words.com
-    key : 'your api key',
+    key : localStorage.getItem('w3wkey'),
     // password is only needed, if you want to use ArcGIS locators, see http://esri.what3words.com/arcgis-setup
-    password: 'your locator password'
+    password: localStorage.getItem('w3w.arcgis.password'),
   };
 });
