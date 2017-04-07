99ea57eb87fa999543880842b0a6272475399cde
arnaud ferrand <arferrand@gmail.com>
2017-04-07 17:06:30 +0200

    update api to 3.20 put back bower components to use with gh-pages

---
+++ js/config_ags.js
@@ -0,0 +1,21 @@
+define([], function() {
+    // Get the user's w3w API key via prompt
+    if (!localStorage.getItem('w3wkey')) {
+      localStorage.setItem(
+        'w3wkey',
+        prompt('What is your w3w API key (w3w arcgis server username)?')
+      );
+    }
+    if (!localStorage.getItem('w3w.arcgis.password')) {
+      localStorage.setItem(
+        'w3w.arcgis.password',
+        prompt('What is your w3w arcgis server password?')
+      );
+    }
+  return {
+    // sign up of for your api key, it is free at http://developer.what3words.com
+    key : localStorage.getItem('w3wkey'),
+    // password is only needed, if you want to use ArcGIS locators, see http://esri.what3words.com/arcgis-setup
+    password: localStorage.getItem('w3w.arcgis.password'),
+  };
+});
